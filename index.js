0e8f3936a34385f44875ffa9f16c27fde0a8b312
Dew613 <dewansunnah613@gmail.com>
2024-01-04 15:42:47 +0000

    flames are different colors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let index = 0, interval = 1000;
 
+const colors = ["#a10100", "#f33c04", "#ffc11f"] 
+
 const rand = (min, max) =>{
 
         return Math.floor(Math.random() *(max-min +1)) + min;
@@ -8,7 +10,9 @@ const rand = (min, max) =>{
 const animate = (fire) => {
     fire.style.setProperty("--fire-left", `${rand(-10,100)}%`);
     fire.style.setProperty("--fire-top", `${rand(-40,80)}%`);
-
+    path = fire.children[0].children[0];
+    console.log(path)
+    path.style.setProperty("--fire-color", colors[Math.floor(Math.random()*colors.length)]);
 
     fire.style.animation = "none";
     fire.offsetHeight;
