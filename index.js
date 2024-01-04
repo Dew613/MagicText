dc2f1be1a95a33f1060fcff938d9bc533fb30de5
Dew613 <dewansunnah613@gmail.com>
2024-01-04 15:23:08 +0000

    set intervals to each fire svg and a timeout to keep the animation going

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const rand = (min, max) =>{
         return Math.floor(Math.random() *(max-min +1)) + min;
 }
 
+const animate = (fire) => {
+    fire.style.setProperty("--fire-left", `${rand(-10,100)}%`);
+    fire.style.setProperty("--fire-top", `${rand(-40,80)}%`);
+
+
+    fire.style.animation = "none";
+    fire.offsetHeight;
+    fire.style.animation = "";
+
+}
+
 for(const fire of document.getElementsByClassName("magic-fire")){
-    setInterval( ()=>{
-        fire.style.setProperty("--fire-left", `${rand(-10,100)}%`);
-        fire.style.setProperty("--fire-top", `${rand(-40,80)}%`);
+    setTimeout( ()=>{
+        animate(fire);
+        setInterval( ()=>{animate(fire)}, 1000);
+    }, index++ * (interval/3));
 
-
-        fire.style.animation = "none";
-        fire.offsetHeight;
-        fire.style.animation = "";
-
-    }, 1000)
 }
